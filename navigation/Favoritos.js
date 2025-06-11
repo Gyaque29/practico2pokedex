3b6acf5d996ba5e0a59a4369d057abdddaf0ccd0
Gyaque29 <gonzaloyaque29@gmail.com>
2025-06-11 10:20:36 -0300

    Commit 8 pequeños arreglos

--- navigation/Favoritos.js
+++ navigation/Favoritos.js
@@ -4,7 +4,7 @@ import { styles } from "../style/styles";
 export default function Settings() {
   return (
     <View style={styles.favoritos}>
-      <Text style={styles.text}>Pantalla de Favoritos</Text>
+      <Text style={styles.text}>No tienes favoritos</Text>
     </View>
   );
 }
